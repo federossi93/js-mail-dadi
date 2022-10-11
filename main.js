1006d9fe991779ed0355381d0aee086b62176377
federossi93 <rossifederico0993@gmail.com>
2022-10-11 18:00:48 +0200

    sostituito break

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 //Chiedi all’utente la sua email.
 let email = prompt('Inserisci la tua email')
 
-
 //controlla che sia nella lista di chi può accedere.
 
 const emailList = ['fede@gmail.com','jack@gmail.com','luca@gmail.com']
 
 let testo = document.querySelector('h1')
 
+let isValid = false
+
 //ciclo da controllare!!!!
 
 for (let i = 0; i < emailList.length; i++){
-
     const emailGiusta = emailList[i]
 
     if(email == emailGiusta){
         console.log('Email coretta');
-        testo.innerHTML = 'bravo'
-        break
-    }
-    else{
-        console.log('Email sbagliata');
-        testo.innerHTML = 'sbagliato'
-    }
-    //stampa un messaggio appropriato sull’esito del controllo.
+        isValid = true
+    }    
 } 
+//stampa un messaggio appropriato sull’esito del controllo.
+if(isValid === true){
+    testo.innerHTML = 'Email corretta'
+}else{
+    testo.innerHTML = 'Email Sbagliata'
+}
 
 //Generare un numero random da 1 a 6,
 //sia per il giocatore sia per il computer.
