ef44069045e66a58a5999af3555c0ab811e2c808
federossi93 <rossifederico0993@gmail.com>
2022-10-11 16:05:41 +0200

    add dice

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Chiedi all’utente la sua email.
+/*//Chiedi all’utente la sua email.
 let email = prompt('Inserisci la tua email')
 
 
@@ -23,7 +23,33 @@ for (let i = 0; i < emailList.length; i++){
         console.log('Email sbagliata');
         testo.innerHTML = 'sbagliato'
     }
+    //stampa un messaggio appropriato sull’esito del controllo.
+} */
 
+//Generare un numero random da 1 a 6,
+//sia per il giocatore sia per il computer.
+//Stabilire il vincitore, in base a chi fa il punteggio più alto.
+//Prima di partire a scrivere codice poniamoci qualche
+//domanda: Che ci sia un array da qualche parte?
+//Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+let giocatore = Math.round(Math.random()* 6 + 1)
+console.log(giocatore);
+
+let computer = Math.round(Math.random()* 6 + 1)
+console.log(computer);
+
+let vincitore = document.querySelector('h2')
+
+let perdente = document.querySelector('h3')
+
+if(giocatore > computer){
+    console.log('giovìcatore vince');
+    vincitore.innerHTML = 'giocatore vince'
+    perdente.innerHTML = giocatore + ' vs ' + computer
+}else{
+    console.log('computer vince');
+    vincitore.innerHTML = 'computer vince'
+    perdente.innerHTML = computer + ' vs ' + giocatore
 }
 
-//stampa un messaggio appropriato sull’esito del controllo.
