3b9df2619947a67441b26abdb808764a19703074
federossi93 <rossifederico0993@gmail.com>
2022-10-11 15:40:50 +0200

    add cilce

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 //Chiedi all’utente la sua email.
-
+let email = prompt('Inserisci la tua email')
 
 
 //controlla che sia nella lista di chi può accedere.
+
+const emailList = ['fede@gmail.com','jack@gmail.com','luca@gmail.com']
+
+let testo = document.querySelector('h1')
+
+//ciclo da controllare!!!!
+
+for (let i = 0; i < emailList.length; i++){
+
+    const emailGiusta = emailList[i]
+
+
+
+    if(email == emailGiusta){
+        console.log('Email coretta');
+        testo.innerHTML = 'bravo'
+        break
+    }else if(email != emailGiusta){
+        console.log('Email sbagliata');
+        testo.innerHTML = 'sbagliato'
+    }
+
+}
+
 
 
 
